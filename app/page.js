a9d7975adf5d6fea842c6425d4f17945a842e488
mahmoud elbalhi <mahmoud01234elbalhi@gmail.com>
2025-07-11 18:21:14 +0300

    up

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="font-creepster text-4xl">This is Creepster via Tailwind!</h1>
-      <p className="font-creepster">Hello world!</p>
+      <p className="font-creepster">Hello world!ffhfjifjigjigjgig</p>
     </main>
   );
 }
