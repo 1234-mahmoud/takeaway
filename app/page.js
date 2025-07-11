62f358f1d6cf7fd182beb280ebdf585699301bb4
Mustafa_Barakat <mustafabarakat633@gmail.com>
2025-07-11 18:24:09 +0300

    new h1

--- app/page.js
+++ app/page.js
@@ -3,8 +3,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1 className="font-creepster text-4xl">This is Creepster via Tailwind!</h1>
+      <h1 className="font-creepster text-4xl">
+        This is Creepster via Tailwind!
+      </h1>
       <p className="font-creepster">Hello world!ffhfjifjigjigjgig</p>
+      <h1>New</h1>
     </main>
   );
 }
