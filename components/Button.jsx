2dad9e5e66f9891ed47960dc29e1630073979159
Mustafa_Barakat <mustafabarakat633@gmail.com>
2025-07-12 23:28:04 +0300

    header

---
+++ components/Button.jsx
@@ -0,0 +1,10 @@
+export default function Button({ children, ...props }) {
+  return (
+    <button
+      {...props}
+      className="bg-amber-400 px-6 py-1.5 rounded-3xl hover:bg-amber-500 duration-200"
+    >
+      {children}
+    </button>
+  );
+}
---
+++ components/Header.jsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import Button from "@/components/Button";
+import { IoMenu } from "react-icons/io5";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
+
+const navLinks = [
+  { text: "home", href: "/" },
+  { text: "menu", href: "menu" },
+  { text: "about", href: "about" },
+  { text: "book table", href: "booking" },
+];
+
+export default function Header() {
+  return (
+    <header>
+      <div className={`container flex justify-between items-center h-14`}>
+        <Link href={""} className="text-xl font-bold">
+          Takeaway
+        </Link>
+        <nav className="hidden lg:block">
+          <ul className="flex gap-5">
+            {navLinks.map((link) => (
+              <li key={link.text}>
+                <Link
+                  href={link.href}
+                  className={`uppercase hover:text-amber-400 duration-300`}
+                >
+                  {link.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="hidden lg:flex items-center gap-3">
+          <div className="flex gap-3 *:hover:text-amber-400">
+            <FaRegUser size={18} />
+            <FaShoppingCart size={20} />
+            <IoSearch size={20} />
+          </div>
+          <Button>Order Online</Button>
+        </div>
+        <button className=" lg:hidden hover:text-amber-400">
+          <IoMenu size={30} />
+        </button>
+      </div>
+    </header>
+  );
+}
